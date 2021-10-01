e3174eaa4bf8ba91741c2ebe76d2bcdaebfb1aca
jeff-k <jeff_k@fastmail.com>
2021-10-01 14:07:22 +0100

    include required binaries to run

--- cleanreads.py
+++ cleanreads.py
@@ -2,6 +2,7 @@ import sys
 import subprocess
 from error import GpasError
 from pathlib import Path
+from os.path import isabs
 
 riak = Path("./readItAndKeep").resolve()
 ref_genome = "MN908947.fasta"
@@ -20,33 +21,61 @@ with subprocess.Popen(
 
 class Decontamination:
     process = None
-    error = []
+    error = None
+    fq1 = None
+    fq2 = None
 
-    def __init__(self, fq1, fq2=None, outdir=None):
+    def __init__(
+        self, fq1, fq2=None, sample="clean_reads", root=Path("./"), outdir=Path("/tmp")
+    ):
         #        if not outdir:
         #            outdir = Tempfile.dir()
         # test output dir
-        outdir = "/tmp"
+
+        #        if not isabs(fq1):
+        if True:
+            fq1 = root / fq1
+            #        if not isabs(fq2):
+            fq2 = root / fq2
 
         if not Path(fq1).exists():
             print("missing", fq1, file=sys.stderr)
             raise GpasError()
+        self.fq1 = Path(outdir) / f"{sample}.reads_1.fastq.gz"
 
         if fq2:
             if not Path(fq2).exists():
                 print("missing", fq2, file=sys.stderr)
                 raise GpasError()
+            self.fq2 = Path(outdir) / f"{sample}.reads_2.fastq.gz"
             # paired run
-
             self.process = subprocess.Popen(
-                [riak, "--ref_fasta", ref_genome, "--reads1", fq1, "--reads2", fq2],
+                [
+                    riak,
+                    "--ref_fasta",
+                    ref_genome,
+                    "--reads1",
+                    fq1,
+                    "--reads2",
+                    fq2,
+                    "--outprefix",
+                    outdir / sample,
+                ],
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
             )
         else:
             # single end
             self.process = subprocess.Popen(
-                [riak, "--ref_fasta", ref_genome, "--reads1", fq1],
+                [
+                    riak,
+                    "--ref_fasta",
+                    ref_genome,
+                    "--reads1",
+                    fq1,
+                    "--outprefix",
+                    outdir / sample,
+                ],
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
             )
@@ -54,7 +83,12 @@ class Decontamination:
     def result(self):
         # wait for decontam process to finish
         out, err = self.process.communicate()
-        if self.process.returncode != 0:
-            #            raise GpasError({"decontamination": self.process.return_code})
-            pass
-        return out, err
+        self.error = err
+        # print(out, file=sys.stderr)
+        if self.process.returncode == 0:
+            if self.fq2:
+                return self.fq1, self.fq2
+            else:
+                return self.fq2
+        else:
+            return None
--- validate.py
+++ validate.py
@@ -9,32 +9,68 @@ from error import GpasError
 
 
 def parse_row(d):
-    result, e = {}, []
+    errors = []
     if "Illumina" not in d["instrument_platform"]:
-        e.append({"instrument": "bad-instrument"})
+        errors.append({"instrument": "bad-instrument"})
 
     fq1_path = Path(d["fastq1"])
     fq2_path = Path(d["fastq2"])
 
     if not fq1_path.exists() or not fq2_path.exists():
-        e.append({"sample": 0, "error": "file-missing"})
+        errors.append({"sample": 0, "error": "file-missing"})
         # raise GpasError({"row": index, "error": "file-missing"})
 
-    print(d, e, file=sys.stderr)
-    return d, e
+    # print(d, e, file=sys.stderr)
+
+    return (
+        {
+            "name": d["name"],
+            "fastq1": d["fastq1"],
+            "fastq2": d["fastq2"],
+            "specimenOrganism": d["specimenOrganism"],
+            "host": d["host"],
+            "collectionDate": d["collectionDate"],
+            "country": d["country"],
+            "submissionTitle": d["submissionTitle"],
+            "submissionDescription": d["submissionDescription"],
+            "status": "Uploaded",
+            "instrument": {
+                "platform": d["instrument_platform"],
+                "model": d["instrument_model"],
+                "flowcell": d["flowcell"],
+            },
+        },
+        errors,
+    )
+
+
+def add_reads(fq1, fq1md5, fq2, fq2md5, batchname):
+    return ({"uri": batchname / fq1.name, "md5": fq1md5},)
+
+
+def batch(batchname, samples):
+    return {
+        "batch": {
+            "fileName": batchname,
+            "uploadedBy": "test",
+            "organisation": "test",
+            "site": "test",
+            "uploadedOn": "2021-09-27T22:27:49.304Z",
+            "samples": samples,
+        }
+    }
 
 
 class Samplesheet:
     samples = []
     batch = None
-    root = None
+    parent = None
 
     def __init__(self, fn, fastq_prefix=None):
         with open(fn, "r") as fd:
-            self.root = fn.root
             reader = csv.DictReader(fd)
             errors = {}  # list of parsing errors indexed by row
-
+            samples = []
             for index, row in enumerate(reader):
                 #                try:
                 rowdata, rowerror = parse_row(row)
@@ -46,8 +82,12 @@ class Samplesheet:
             # propagate parsing errors
             #    raise GpasError(errors)
 
-            missing_files = []
-        self.batch = str(fn.name)
+            self.batch = fn.stem
 
-    def to_json(self):
-        return {"batch": self.batch, "samples": self.samples}
+    def make_submission(self, samples):
+        good_samples = []
+        for sample in samples:
+            good_samples.add(
+                {"pe_reads": add_reads(fq1, fq1md5, fq2, fq2md5, self.batch)}
+            )
+        return {"submission": batch(self.batchname, good_samples)}
