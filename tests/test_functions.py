dc9a00c4f2351a0b4e1ab072cab305c611788d87
Denis Volk <denis.volk@gmail.com>
2021-11-22 16:35:38 +0000

    added json flag to fn to allow unit tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,4 +1,8 @@
-import pytest, validate
+import pytest, validate, imp
+
+from cleanreads import Decontamination
+
+gpas_uploader = imp.load_source('gpas-uploader','./gpas-uploader')
 
 from pathlib import Path
 
@@ -8,12 +12,33 @@ def test_validate_illumina_spreadsheet():
 
     validss = validate.Samplesheet(samplesheet)
 
+    print(validss.validate())
     assert validss.validate()['validation']['status'] == 'completed'
 
 def test_validate_nanopore_spreadsheet():
 
-    samplesheet = Path('examples/nanopore-samplesheet-template-good.csv')
+    samplesheet2 = Path('examples/nanopore-samplesheet-template-good.csv')
 
-    validss = validate.Samplesheet(samplesheet)
+    validss = validate.Samplesheet(samplesheet2)
 
+    print(validss.validate())
     assert validss.validate()['validation']['status'] == 'completed'
+
+def test_decontaminate_illumina_spreadsheet():
+
+    Decontamination('examples/MN908947_1.fastq.gz',sample='MN908947')
+    
+    samplesheet = Path('examples/illumina-samplesheet-template-good.csv')
+
+    validation = validate.Samplesheet(samplesheet)
+
+    assert validation.validate()['validation']['status'] == 'completed'
+
+    for sample in validation.samples:
+
+        if sample.fq2:
+            gpas_uploader.submit_illumina(sample, use_json=True)
+            
+        print(json.dumps(validation.make_submission()))
+
+
