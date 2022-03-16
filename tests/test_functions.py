371f0051de7a2368a862281495dc27d7308eb376
Philip W Fowler <philipwfowler@users.noreply.github.com>
2022-03-16 08:33:58 +0000

    adding samtools/riak to dist in GHActions

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -5,55 +5,7 @@ import subprocess
 import importlib
 import sys
 
-# def test_spreadsheet_validation_correctly_fails():
-#
-#     with pytest.raises(Exception) as e_info:
-#         validate.Batch('examples/none-existent-file.csv')
-#
-#     # forgot file extension
-#     with pytest.raises(Exception) as e_info:
-#         validate.Batch('examples/illumina-samplesheet-template-good')
-#
-# def test_validate_illumina_spreadsheet():
-#
-#     samplesheet = pathlib.Path('examples/illumina-samplesheet-template-good.csv')
-#
-#     validss = validate.Batch(samplesheet)
-#
-#     assert validss.validate()['validation']['status'] == 'completed'
-#
-# def test_validate_bam_illumina_spreadsheet(tmp_path):
-#
-#     shutil.copyfile('examples/bam-nanopore-samplesheet-template-good.csv', tmp_path / 'test-nanopore.csv')
-#
-#     shutil.copyfile('examples/reference.bam', tmp_path / 'reference.bam')
-#
-#     samplesheet = pathlib.Path(tmp_path / 'test-nanopore.csv')
-#
-#     validss = validate.Batch(samplesheet)
-#
-#     assert validss.validate()['validation']['status'] == 'completed'
-#
-# def test_validate_nanopore_spreadsheet():
-#
-#     samplesheet = pathlib.Path('examples/nanopore-samplesheet-template-good.csv')
-#
-#     validss = validate.Batch(samplesheet)
-#
-#     assert validss.validate()['validation']['status'] == 'completed'
-#
-# def test_validate_bam_illumina_spreadsheet(tmp_path):
-#
-#     shutil.copyfile('examples/bam-nanopore-samplesheet-template-good.csv', tmp_path / 'test-illumina.csv')
-#
-#     shutil.copyfile('examples/reference.bam', tmp_path / 'reference.bam')
-#
-#     samplesheet = pathlib.Path(tmp_path / 'test-illumina.csv')
-#
-#     validss = validate.Batch(samplesheet)
-#
-#     assert validss.validate()['validation']['status'] == 'completed'
-#
+
 def test_riak_ok(tmp_path):
 
     if pathlib.Path("./readItAndKeep").exists():
