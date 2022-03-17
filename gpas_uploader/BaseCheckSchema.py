55b3e2d312c3a10e65db709e14fbeb1b51c7563c
Philip W Fowler <philipwfowler@users.noreply.github.com>
2022-03-17 12:54:28 +0000

    allow space in district field

--- gpas_uploader/BaseCheckSchema.py
+++ gpas_uploader/BaseCheckSchema.py
@@ -66,7 +66,7 @@ class BaseCheckSchema(pandera.SchemaModel):
 
     region: Series[str] = pandera.Field(nullable=True, isin=[i.name for i in list(pycountry.subdivisions)], coerce=True)
 
-    district: Series[str] = pandera.Field(str_matches=r'^[A-Za-z0-9:_-]+$', nullable=True, coerce=True)
+    district: Series[str] = pandera.Field(str_matches=r'^[\sA-Za-z0-9:_-]+$', nullable=True, coerce=True)
 
     # insist that the tags is alphanumeric, including : as it is the delimiter
     tags: Series[str] = pandera.Field(nullable=True, str_matches=r'^[A-Za-z0-9:_-]+$', coerce=True)
